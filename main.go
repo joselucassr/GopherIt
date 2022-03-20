d795004fab52f1eff6551845a2a4a10d91c2e7bc
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-20 04:58:06 -0300

    Doguinho.

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World!")
+	fmt.Println("  __      _")
+  fmt.Println("o'')}____//")
+  fmt.Println(" `_/      )")
+  fmt.Println(" (_(_/-(_/")
 }
