187d7a4603339ad7e023498006c1518a04d776a9
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-20 05:14:35 -0300

    X-Wing.

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
+// Fonte: https://www.asciiart.eu/movies/star-wars
 func main() {
-	fmt.Println("  __      _")
-  fmt.Println("o'')}____//")
-  fmt.Println(" `_/      )")
-  fmt.Println(" (_(_/-(_/")
+	fmt.Println("           __")
+	fmt.Println(".-.__      \\ .-.  ___  __")
+	fmt.Println("|_|  '--.-.-(   \\/\\;;\\_\\.-._______.-.")
+	fmt.Println("(-)___     \\ \\ .-\\ \\;;\\(   \\       \\ \\")
+	fmt.Println(" Y    '---._\\_((Q)) \\;;\\ .-\\     __(_)")
+	fmt.Println(" I           __'-' / .--.((Q))---'    \\,")
+	fmt.Println(" I     ___.-:    \\|  |   \\'-'_          \\")
+	fmt.Println(" A  .-'      \\ .-.\\   \\   \\ \\ '--.__     '\\")
+	fmt.Println(" |  |____.----((Q))\\   \\__|--\\_      \\     '")
+	fmt.Println("    ( )        '-'  \\_  :  \\-' '--.___\\")
+	fmt.Println("     Y                \\  \\  \\       \\(_)")
+	fmt.Println("     I                 \\  \\  \\         \\,")
+	fmt.Println("     I                  \\  \\  \\          \\")
+	fmt.Println("     A                   \\  \\  \\          '\\")
+	fmt.Println("     |              snd   \\  \\__|           '")
+	fmt.Println("                           \\_:.  \\")
+	fmt.Println("                             \\ \\  \\")
+	fmt.Println("                              \\ \\  \\")
+	fmt.Println("                               \\_\\_|")
 }
